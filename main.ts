a85fb749be9ce900e5dd30cb149ec95e157fc1f0
mode80 <github@tihan.com>
2013-03-10 23:34:38 -0700

    barely there calculator functionality

---
+++ main.ts
@@ -0,0 +1,22 @@
+﻿/// <reference path="d3.d.ts" />
+
+  var eval_me = "" 
+  var VALID = "1234567890%*()-+/"
+
+  var svg = d3.select('svg')
+  var body = d3.select('body')
+  var txt = svg.append('text')
+    .attr({
+      x: '50%',
+      y: '50%' /**/})  
+
+  body.on('keypress',(d,i)=>{
+    var char_code = (<any>d3.event).which
+    var char = String.fromCharCode(char_code)
+    if (char === 'c') // clear it
+      txt.text(eval_me = "")
+    else if (char === '=' || char_code === 13) { // give answer
+      txt.text(eval(eval_me))
+      eval_me = "" /**/}
+    else if (VALID.indexOf(char) >= 0) // append to input
+      txt.text(eval_me += char) /**/})
