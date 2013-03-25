a1890a279dea241250318e8cb31565014332a903
mode80 <github@tihan.com>
2013-03-25 13:55:39 -0700

    correct animation of digit places -- but slow @ 10000

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ module calcsand {
   // expression related vars
   var eval_me = ""
   var data = []
-  var term = []; term[1] = "", term[2] = ""
+  var term1 = "", term2 = ""
   var operator = ""
   var answer = ""
   var after_operator = false
@@ -49,12 +49,12 @@ module calcsand {
 
   }
 
-  function recenterDisplay(duration=250) {
-    var digits_wide = Math.max(term[1].length, term[2].length)
-    var digits_high = term[2].length ? 2 : 1
+  function recenterDisplay(duration=0) {
+    var digits_wide = Math.max(term1.length, term2.length)
+    var digits_high = term2.length ? 2 : 1
     if (answer.length) { digits_wide = answer.length; digits_high = 1 }
-    var x_offset = svg_half_w - ( digits_wide * digit_half_w ) 
-    var y_offset = svg_half_h - (digits_high * digit_half_h) - (digits_high - 1) * digit_y_margin 
+    var x_offset = Math.round( svg_half_w - ( digits_wide * digit_half_w ) )
+    var y_offset = Math.round( svg_half_h - (digits_high * digit_half_h) - (digits_high - 1) * digit_y_margin )
     display
       .transition()
       .duration(duration)
@@ -62,8 +62,8 @@ module calcsand {
   }
 
   function resizeDigits() {
-    digit_w = svg_w / Math.max(term[1].length+1, term[2].length+1, answer.length+1)  
-    digit_h = svg_h / (answer.length? 1 : term[2].length ? 2 : 1) 
+    digit_w = svg_w / Math.max(term1.length+1, term2.length+1, answer.length+1)  
+    digit_h = svg_h / (answer.length? 1 : term2.length ? 2 : 1) 
     digit_x_margin = 0
     digit_y_margin = digit_h * 0.15
     digit_w -= digit_x_margin * 2
@@ -76,7 +76,7 @@ module calcsand {
     digit_half_h = digit_h / 2
     digit_full_w = digit_w + (digit_x_margin*2)      
     digit_full_h = digit_h + (digit_y_margin*2)     
-    resizeStroke(digit_w * 0.05)
+    resizeStroke(Math.round(digit_w * 0.05))
   }
 
   function resizeStroke(width: number) { 
@@ -107,14 +107,14 @@ module calcsand {
     }
     if (isDigit(input)) {
       if (answer.length) resetToStart()
-      if (after_operator) {term[2] += input} else { term[1] += input }
+      if (after_operator) {term2 += input} else { term1 += input }
       showTerms()
     }
     else if (isOperator(input)) {
       if (answer) {
         var prev_answer = answer
         resetToStart()
-        term[1] = prev_answer
+        term1 = prev_answer
         showTerms()
       }
       operator = input
@@ -122,13 +122,13 @@ module calcsand {
       showOperator(input)
     }
     else if (input == INPUT.EQUALS) {
-      answer = eval(term[1]+operator+term[2]) + ""
+      answer = eval(term1+operator+term2) + ""
       showAnswer()
     }
   }
 
   function resetToStart() {
-    term[1] = ""; term[2] = ""
+    term1 = ""; term2 = ""
     operator = ""; after_operator = false
     eval_me = ""; data = []
     answer = ""; 
@@ -143,7 +143,7 @@ module calcsand {
     // reset digit sizing 
       resizeDigits()
     // regenerate line data for term array 
-      data = makeRenderingData(term)
+      data = makeRenderingData(term1, term2)
     // rerender new data
       renderData(data)
       recenterDisplay()
@@ -155,12 +155,12 @@ module calcsand {
 
   function showAnswer() {
     resizeDigits()
-    data = makeRenderingData(["",answer,""]) // TODO fix this ugly cludge
-    recenterDisplay(1000)
-    renderData(data,1000)
+    data = makeRenderingData(answer) 
+    recenterDisplay(3000)
+    renderData(data,3000)
   }
 
-  function renderData(data:{}[], duration:number=250) {
+  function renderData(data:{}[], duration:number=0) {
     var lines = display.selectAll('line').data(data)
     lines
       .transition()
@@ -169,6 +169,7 @@ module calcsand {
       .attr('x2', (d) => { return d.x2 })
       .attr('y1', (d) => { return d.y1 })
       .attr('y2', (d) => { return d.y2 })
+      .attr('opacity', (d) => { return d.o })
       .attr('transform', (d) => { return 'translate(' + d.xoff + ',' + d.yoff + ')' })
     lines
       .enter()
@@ -177,123 +178,134 @@ module calcsand {
       .attr('x2', (d) => { return d.x2 })
       .attr('y1', (d) => { return d.y1 })
       .attr('y2', (d) => { return d.y2 })
+      .attr('opacity', (d) => { return d.o })
       .attr('transform', (d) => { return 'translate(' + d.xoff + ',' + d.yoff + ')' })
-      //.attr('opacity', 0)
-      //.transition()
-      .attr('opacity', 1)
     lines
       .exit()
       .remove()
   }
 
-  function makeRenderingData(term:string[]) {
-      
-    var data:any = []
+  function makeRenderingData(part1="", part2="") {
+    // takes an array of expression parts (e.g. term1, term2, answer) and returns an array of data suitable for rendering 
+
+    var data: any = []
  
     // make it easy to interchange push and unshift 
     data.grow = Array.prototype.unshift 
 
     // pad shortest term with spaces to keep same-significant digits in sync
-    term[1] = Array(Math.max(term[2].length-term[1].length+1,0)).join(" ") + term[1]
-    term[2] = Array(Math.max(term[1].length-term[2].length+1)).join(" ") + term[2]
+    part1 = Array(Math.max(part2.length - part1.length + 1, 0)).join(" ") + (part1 + "")
+    part2 = Array(Math.max(part1.length - part2.length + 1, 0)).join(" ") + (part2 + "")
 
-    // take each digit from each term in turn
+    // package parts for rendering into an array that we can iterate over
+    var parts = [part1,part2]
+
+    // take each digit from each part in turn
     var digit_inc = 0  // this will count up
-    var digit_i = Math.max(term[1].length,term[2].length) // this will count down
+    var max_digits = Math.max(part1.length,part2.length)
+    var digit_i = max_digits // this will count down
+    var multiplier = 1 // this goes up to 10 after 1st digit
     while (digit_i--) { // loop through each digit, starting with least significant 
       digit_inc++
-      var term_i = term.length - 1 
-      while (term_i--) { // loop (backwards) through each term
-        var term_n = term_i+1 // term_i is a 0-based index whereas term_n is the 1-based ordinal
-        var digit = term[term_n].substr(digit_i,1)
-        var x_offset = (digit_full_w * (digit_i) )
-        var y_offset = (digit_full_h * term_i) 
-        switch (digit) {
-          case "", " ":
-            break
-          case ".":
-            data.grow({ x1: x(50), y1: y(100), x2: x(50), y2: y(100), xoff: x_offset, yoff: y_offset })
-            break
-          case "0":
-            data.grow({ x1: x(50), y1: y(50), x2: x(50), y2: y(50), xoff: x_offset, yoff: y_offset })
-            break
-          case "1":
-            data.grow({ x1: x(50), y1: y(00), x2: x(50), y2: y(99), xoff: x_offset, yoff: y_offset })
-            break
-          case "2":
-            data.grow({ x1: x(80), y1: y(00), x2: x(20), y2: y(93), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(23), y1: y(99), x2: x(80), y2: y(99), xoff: x_offset, yoff: y_offset })
-            break
-          case "3":
-            data.grow({ x1: x(20), y1: y(00), x2: x(75), y2: y(44), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(73), y1: y(50), x2: x(20), y2: y(50), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(75), y1: y(56), x2: x(20), y2: y(99), xoff: x_offset, yoff: y_offset })
-            break
-          case "4":
-            data.grow({ x1: x(20), y1: y(00), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(80), y1: y(55), x2: x(80), y2: y(99), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(80), y1: y(00), x2: x(80), y2: y(45), xoff: x_offset, yoff: y_offset })
-            break
-          case "5":
-            data.grow({ x1: x(80), y1: y(00), x2: x(25), y2: y(00), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(20), y1: y(05), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(80), y1: y(55), x2: x(80), y2: y(95), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(75), y1: y(99), x2: x(20), y2: y(99), xoff: x_offset, yoff: y_offset })
-            break
-          case "6":
-            data.grow({ x1: x(80), y1: y(00), x2: x(25), y2: y(00), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(20), y1: y(05), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(80), y1: y(55), x2: x(80), y2: y(94), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(75), y1: y(99), x2: x(25), y2: y(99), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(20), y1: y(94), x2: x(20), y2: y(55), xoff: x_offset, yoff: y_offset })
-            break
-          case "7":
-            data.grow({ x1: x(20), y1: y(25), x2: x(20), y2: y(05), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(25), y1: y(00), x2: x(76), y2: y(00), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(80), y1: y(06), x2: x(35), y2: y(99), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(80), y1: y(55), x2: x(80), y2: y(70), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(75), y1: y(75), x2: x(25), y2: y(75), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(20), y1: y(70), x2: x(20), y2: y(55), xoff: x_offset, yoff: y_offset })
-            break
-          case "8":
-            data.grow({ x1: x(55), y1: y(00), x2: x(75), y2: y(20), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(75), y1: y(30), x2: x(55), y2: y(50), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(45), y1: y(50), x2: x(25), y2: y(30), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(25), y1: y(20), x2: x(45), y2: y(00), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(25), y1: y(61), x2: x(75), y2: y(61), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(80), y1: y(66), x2: x(80), y2: y(94), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(75), y1: y(99), x2: x(25), y2: y(99), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(20), y1: y(94), x2: x(20), y2: y(66), xoff: x_offset, yoff: y_offset })
-            break
-          case "9":
-            data.grow({ x1: x(75), y1: y(00), x2: x(25), y2: y(00), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(20), y1: y(05), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(80), y1: y(45), x2: x(80), y2: y(05), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(54), y1: y(05), x2: x(70), y2: y(22), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(70), y1: y(28), x2: x(54), y2: y(45), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(48), y1: y(45), x2: x(31), y2: y(28), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(31), y1: y(22), x2: x(48), y2: y(05), xoff: x_offset, yoff: y_offset })
-            data.grow({ x1: x(80), y1: y(55), x2: x(50), y2: y(99), xoff: x_offset, yoff: y_offset })
-            break
-        } // end switch
+      var part_i = parts.length 
+      var opacity = 1 //Math.max(digit_inc / max_digits , 0.1)
+      while (part_i--) { // loop (backwards) through each term
+        var digit = parts[part_i].substr(digit_i,1)
+        var x_offset = Math.round( (digit_full_w * (digit_i) ) )
+        var y_offset = Math.round( (digit_full_h * part_i) )
+        var mult_i = multiplier
+        while (mult_i--) {
+          switch (digit) {
+            case "", " ":
+              break
+            case ".":
+              //data.grow({ x1: x(50), y1: y(99), x2: x(50), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              break
+            case "0":
+              //data.grow({ x1: x(50), y1: y(50), x2: x(50), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              break
+            case "1":
+              data.grow({ x1: x(50), y1: y(00), x2: x(50), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              break
+            case "2":
+              data.grow({ x1: x(80), y1: y(00), x2: x(20), y2: y(93), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(23), y1: y(99), x2: x(80), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              break
+            case "3":
+              data.grow({ x1: x(20), y1: y(00), x2: x(75), y2: y(44), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(73), y1: y(50), x2: x(20), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(75), y1: y(56), x2: x(20), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              break
+            case "4":
+              data.grow({ x1: x(20), y1: y(00), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(80), y1: y(55), x2: x(80), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(80), y1: y(00), x2: x(80), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
+              break
+            case "5":
+              data.grow({ x1: x(80), y1: y(00), x2: x(25), y2: y(00), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(20), y1: y(05), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(80), y1: y(55), x2: x(80), y2: y(95), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(75), y1: y(99), x2: x(20), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              break
+            case "6":
+              data.grow({ x1: x(80), y1: y(00), x2: x(25), y2: y(00), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(20), y1: y(05), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(80), y1: y(55), x2: x(80), y2: y(94), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(75), y1: y(99), x2: x(25), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(20), y1: y(94), x2: x(20), y2: y(55), xoff: x_offset, yoff: y_offset, o: opacity })
+              break
+            case "7":
+              data.grow({ x1: x(20), y1: y(25), x2: x(20), y2: y(05), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(25), y1: y(00), x2: x(76), y2: y(00), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(80), y1: y(06), x2: x(35), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(80), y1: y(55), x2: x(80), y2: y(70), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(75), y1: y(75), x2: x(25), y2: y(75), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(20), y1: y(70), x2: x(20), y2: y(55), xoff: x_offset, yoff: y_offset, o: opacity })
+              break
+            case "8":
+              data.grow({ x1: x(55), y1: y(00), x2: x(75), y2: y(20), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(75), y1: y(30), x2: x(55), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(45), y1: y(50), x2: x(25), y2: y(30), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(25), y1: y(20), x2: x(45), y2: y(00), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(25), y1: y(61), x2: x(75), y2: y(61), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(80), y1: y(66), x2: x(80), y2: y(94), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(75), y1: y(99), x2: x(25), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(20), y1: y(94), x2: x(20), y2: y(66), xoff: x_offset, yoff: y_offset, o: opacity })
+              break
+            case "9":
+              data.grow({ x1: x(75), y1: y(00), x2: x(25), y2: y(00), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(20), y1: y(05), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(80), y1: y(45), x2: x(80), y2: y(05), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(54), y1: y(05), x2: x(70), y2: y(22), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(70), y1: y(28), x2: x(54), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(48), y1: y(45), x2: x(31), y2: y(28), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(31), y1: y(22), x2: x(48), y2: y(05), xoff: x_offset, yoff: y_offset, o: opacity })
+              data.grow({ x1: x(80), y1: y(55), x2: x(50), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              break
+          } // end switch
 
-      } // end digit_i loop
-    } // end term_n loop 
+        } // end mult_i loop
 
-    term[1] = term[1].trim()
-    term[2] = term[2].trim()
+      } // end part_i loop
+
+      multiplier *= 10
+
+    } // end digit_i loop 
+
+    part1 = part1.trim()
+    part2 = part2.trim()
     
     return data
 
   } // end function 
 
-  function x(d) { return d * digit_w/100 }
-  function y(d) { return d * digit_h/100 }
+  function x(d) { return Math.round(d * digit_w/100) }
+  function y(d) { return Math.round(d * digit_h/100) }
 
   function isDigit(char: string): bool { return ("1234567890".indexOf(char) >= 0) }
   function isOperator(char: string): bool { return ("*-+/".indexOf(char) >= 0) }
