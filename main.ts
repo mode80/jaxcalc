101eff339854d5d07bfda5b75f35e4afda4e5986
mode80 <github@tihan.com>
2013-03-25 17:42:53 -0700

    decimals, continuation operations

--- main.ts
+++ main.ts
@@ -4,15 +4,19 @@ module calcsand {
 
   // expression related vars
   var eval_me = ""
-  var data = []
   var term1 = "", term2 = ""
   var operator = ""
   var answer = ""
   var after_operator = false
 
+  // rendering related vars
+  var lines:Object[] = []
+  var ellipses:Object[] = []
+
   // valid input "enum"
   var INPUT = {
     0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '10',
+    DECIMAL: '.',
     ADD: '+',
     SUBTRACT: '-',
     MULTIPLY: '*',
@@ -33,7 +37,7 @@ module calcsand {
   // d3 selection vars
   var body = d3.select('body')
   var svg = body.append('svg').attr({ width: svg_w, height: svg_h })
-  var display = svg.append('g').attr({ height: 1, width: 1 })
+  var display = svg.append('g').attr({ 'class': 'display', height: 1, width: 1 })
   var sheet:any = document.styleSheets[0]
 
   main()
@@ -81,10 +85,10 @@ module calcsand {
 
   function resizeStroke(width: number) { 
     if (typeof stroke_rule === "undefined") {
-      var rule_num = sheet.insertRule('svg line {}', sheet.rules.length) 
+      var rule_num = sheet.insertRule('.display {}', sheet.rules.length) 
       stroke_rule = sheet.rules[rule_num]
     }
-    stroke_rule.cssText = 'svg line { stroke-width : ' + Math.floor(width) + '}'
+    stroke_rule.cssText = '.display { stroke-width : ' + Math.floor(width) + '}'
     stroke_rule.style.strokeWidth = Math.max(1,Math.floor(width)) + "px"
   }
 
@@ -111,31 +115,54 @@ module calcsand {
       showTerms()
     }
     else if (isOperator(input)) {
-      if (answer) {
+      if (answer) { // there exists a previous answer we're operating on
         var prev_answer = answer
         resetToStart()
         term1 = prev_answer
         showTerms()
+        operator = input
+        after_operator = true
       }
-      operator = input
-      after_operator = true
-      showOperator(input)
+      else if (after_operator && term2.length) { // they have everything to hit equals but hit operator instead
+        findAnswer()
+        showAnswer()
+        term1 = answer
+        term2 = "" 
+        answer = ""
+        operator = input
+        after_operator = true
+        showTerms()
+      } else {
+        operator = input
+        after_operator = true
+      }
+     showOperator(input)
     }
     else if (input == INPUT.EQUALS) {
-      answer = eval(term1+operator+term2) + ""
+      findAnswer()
       showAnswer()
     }
   }
 
+  function findAnswer() {
+      try {
+        answer = eval(term1 + operator + term2) + ""
+      } catch (e) {
+        answer = "" 
+      }
+  }
+  
   function resetToStart() {
     term1 = ""; term2 = ""
     operator = ""; after_operator = false
-    eval_me = ""; data = []
+    eval_me = "";
+    lines = []; ellipses = []
     answer = ""; 
   }
 
   function showStart() {
     svg.selectAll('line').remove()
+    svg.selectAll('ellipse').remove()
   }
 
   function showTerms() {
@@ -143,9 +170,9 @@ module calcsand {
     // reset digit sizing 
       resizeDigits()
     // regenerate line data for term array 
-      data = makeRenderingData(term1, term2)
+      makeRenderingData(term1, term2)
     // rerender new data
-      renderData(data)
+      renderData()
       recenterDisplay()
   }
 
@@ -155,14 +182,14 @@ module calcsand {
 
   function showAnswer() {
     resizeDigits()
-    data = makeRenderingData(answer) 
-    recenterDisplay(3000)
-    renderData(data,3000)
+    makeRenderingData(answer) 
+    recenterDisplay(1000)
+    renderData(1000)
   }
 
-  function renderData(data:{}[], duration:number=0) {
-    var lines = display.selectAll('line').data(data)
-    lines
+  function renderData(duration=0) {
+    var svg_lines = display.selectAll('line').data(lines)
+    svg_lines 
       .transition()
       .duration(duration)
       .attr('x1', (d) => { return d.x1 })
@@ -171,7 +198,7 @@ module calcsand {
       .attr('y2', (d) => { return d.y2 })
       .attr('opacity', (d) => { return d.o })
       .attr('transform', (d) => { return 'translate(' + d.xoff + ',' + d.yoff + ')' })
-    lines
+    svg_lines 
       .enter()
       .append('line')
       .attr('x1', (d) => { return d.x1 })
@@ -180,19 +207,40 @@ module calcsand {
       .attr('y2', (d) => { return d.y2 })
       .attr('opacity', (d) => { return d.o })
       .attr('transform', (d) => { return 'translate(' + d.xoff + ',' + d.yoff + ')' })
-    lines
+    svg_lines 
       .exit()
       .remove()
-  }
+    var svg_ellipses = display.selectAll('ellipse').data(ellipses)
+    svg_ellipses 
+      .transition()
+      .duration(duration)
+      .attr('cx', (d) => { return d.cx })
+      .attr('cy', (d) => { return d.cy })
+      .attr('rx', (d) => { return d.rx })
+      .attr('ry', (d) => { return d.ry })
+      .attr('opacity', (d) => { return d.o })
+      .attr('transform', (d) => { return 'translate(' + d.xoff + ',' + d.yoff + ')' })
+    svg_ellipses 
+      .enter()
+      .append('ellipse')
+      .attr('cx', (d) => { return d.cx })
+      .attr('cy', (d) => { return d.cy })
+      .attr('rx', (d) => { return d.rx })
+      .attr('ry', (d) => { return d.ry })
+      .attr('opacity', (d) => { return d.o })
+      .attr('transform', (d) => { return 'translate(' + d.xoff + ',' + d.yoff + ')' })
+    svg_ellipses 
+      .exit()
+      .remove()
+   }
 
   function makeRenderingData(part1="", part2="") {
-    // takes an array of expression parts (e.g. term1, term2, answer) and returns an array of data suitable for rendering 
+    // takes expression parts (e.g. term1, term2, answer) and builds arrays of svg data for later rendering 
 
-    var data: any = []
- 
-    // make it easy to interchange push and unshift 
-    data.grow = Array.prototype.unshift 
-
+    // reset globals 
+    lines = []
+    ellipses = []
+    
     // pad shortest term with spaces to keep same-significant digits in sync
     part1 = Array(Math.max(part2.length - part1.length + 1, 0)).join(" ") + (part1 + "")
     part2 = Array(Math.max(part1.length - part2.length + 1, 0)).join(" ") + (part2 + "")
@@ -219,73 +267,73 @@ module calcsand {
             case "", " ":
               break
             case ".":
-              //data.grow({ x1: x(50), y1: y(99), x2: x(50), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              ellipses.unshift({ cx: x(50), cy: y(99), rx: x(1), ry: y(1), xoff: x_offset, yoff: y_offset, o: opacity })
               break
             case "0":
-              //data.grow({ x1: x(50), y1: y(50), x2: x(50), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              ellipses.unshift({ cx: x(50), cy: y(50), rx: x(40), ry: y(40), xoff: x_offset, yoff: y_offset, o: opacity })
               break
             case "1":
-              data.grow({ x1: x(50), y1: y(00), x2: x(50), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(50), y1: y(00), x2: x(50), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
               break
             case "2":
-              data.grow({ x1: x(80), y1: y(00), x2: x(20), y2: y(93), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(23), y1: y(99), x2: x(80), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(80), y1: y(00), x2: x(20), y2: y(93), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(23), y1: y(99), x2: x(80), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
               break
             case "3":
-              data.grow({ x1: x(20), y1: y(00), x2: x(75), y2: y(44), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(73), y1: y(50), x2: x(20), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(75), y1: y(56), x2: x(20), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(20), y1: y(00), x2: x(75), y2: y(44), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(73), y1: y(50), x2: x(20), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(75), y1: y(56), x2: x(20), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
               break
             case "4":
-              data.grow({ x1: x(20), y1: y(00), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(80), y1: y(55), x2: x(80), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(80), y1: y(00), x2: x(80), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(20), y1: y(00), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(80), y1: y(55), x2: x(80), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(80), y1: y(00), x2: x(80), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
               break
             case "5":
-              data.grow({ x1: x(80), y1: y(00), x2: x(25), y2: y(00), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(20), y1: y(05), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(80), y1: y(55), x2: x(80), y2: y(95), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(75), y1: y(99), x2: x(20), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(80), y1: y(00), x2: x(25), y2: y(00), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(20), y1: y(05), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(80), y1: y(55), x2: x(80), y2: y(95), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(75), y1: y(99), x2: x(20), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
               break
             case "6":
-              data.grow({ x1: x(80), y1: y(00), x2: x(25), y2: y(00), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(20), y1: y(05), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(80), y1: y(55), x2: x(80), y2: y(94), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(75), y1: y(99), x2: x(25), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(20), y1: y(94), x2: x(20), y2: y(55), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(80), y1: y(00), x2: x(25), y2: y(00), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(20), y1: y(05), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(80), y1: y(55), x2: x(80), y2: y(94), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(75), y1: y(99), x2: x(25), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(20), y1: y(94), x2: x(20), y2: y(55), xoff: x_offset, yoff: y_offset, o: opacity })
               break
             case "7":
-              data.grow({ x1: x(20), y1: y(25), x2: x(20), y2: y(05), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(25), y1: y(00), x2: x(76), y2: y(00), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(80), y1: y(06), x2: x(35), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(80), y1: y(55), x2: x(80), y2: y(70), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(75), y1: y(75), x2: x(25), y2: y(75), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(20), y1: y(70), x2: x(20), y2: y(55), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(20), y1: y(25), x2: x(20), y2: y(05), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(25), y1: y(00), x2: x(76), y2: y(00), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(80), y1: y(06), x2: x(35), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(80), y1: y(55), x2: x(80), y2: y(70), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(75), y1: y(75), x2: x(25), y2: y(75), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(20), y1: y(70), x2: x(20), y2: y(55), xoff: x_offset, yoff: y_offset, o: opacity })
               break
             case "8":
-              data.grow({ x1: x(55), y1: y(00), x2: x(75), y2: y(20), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(75), y1: y(30), x2: x(55), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(45), y1: y(50), x2: x(25), y2: y(30), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(25), y1: y(20), x2: x(45), y2: y(00), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(25), y1: y(61), x2: x(75), y2: y(61), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(80), y1: y(66), x2: x(80), y2: y(94), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(75), y1: y(99), x2: x(25), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(20), y1: y(94), x2: x(20), y2: y(66), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(55), y1: y(00), x2: x(75), y2: y(20), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(75), y1: y(30), x2: x(55), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(45), y1: y(50), x2: x(25), y2: y(30), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(25), y1: y(20), x2: x(45), y2: y(00), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(25), y1: y(61), x2: x(75), y2: y(61), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(80), y1: y(66), x2: x(80), y2: y(94), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(75), y1: y(99), x2: x(25), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(20), y1: y(94), x2: x(20), y2: y(66), xoff: x_offset, yoff: y_offset, o: opacity })
               break
             case "9":
-              data.grow({ x1: x(75), y1: y(00), x2: x(25), y2: y(00), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(20), y1: y(05), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(80), y1: y(45), x2: x(80), y2: y(05), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(54), y1: y(05), x2: x(70), y2: y(22), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(70), y1: y(28), x2: x(54), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(48), y1: y(45), x2: x(31), y2: y(28), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(31), y1: y(22), x2: x(48), y2: y(05), xoff: x_offset, yoff: y_offset, o: opacity })
-              data.grow({ x1: x(80), y1: y(55), x2: x(50), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(75), y1: y(00), x2: x(25), y2: y(00), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(20), y1: y(05), x2: x(20), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(25), y1: y(50), x2: x(75), y2: y(50), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(80), y1: y(45), x2: x(80), y2: y(05), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(54), y1: y(05), x2: x(70), y2: y(22), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(70), y1: y(28), x2: x(54), y2: y(45), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(48), y1: y(45), x2: x(31), y2: y(28), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(31), y1: y(22), x2: x(48), y2: y(05), xoff: x_offset, yoff: y_offset, o: opacity })
+              lines.unshift({ x1: x(80), y1: y(55), x2: x(50), y2: y(99), xoff: x_offset, yoff: y_offset, o: opacity })
               break
           } // end switch
 
@@ -293,21 +341,19 @@ module calcsand {
 
       } // end part_i loop
 
-      multiplier *= 10
+      multiplier = 10
 
     } // end digit_i loop 
 
     part1 = part1.trim()
     part2 = part2.trim()
-    
-    return data
 
   } // end function 
 
   function x(d) { return Math.round(d * digit_w/100) }
   function y(d) { return Math.round(d * digit_h/100) }
 
-  function isDigit(char: string): bool { return ("1234567890".indexOf(char) >= 0) }
+  function isDigit(char: string): bool { return ("1234567890.".indexOf(char) >= 0) }
   function isOperator(char: string): bool { return ("*-+/".indexOf(char) >= 0) }
 
 
